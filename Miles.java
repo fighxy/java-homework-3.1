0c0387d502cb462714fef4322bd0fd08b1bf40de
Ivan Kolotov <57476075+kawasary@users.noreply.github.com>
2020-11-27 11:30:58 +0700

    Update Miles.java

--- Miles.java
+++ Miles.java
@@ -1,8 +1,8 @@
 public class Miles {
     public static void main(String[] args) {
-        int ticket_price = 4000; // 4 тыс рублей за билет
-        int bonus_miles_value = 20; // количество начисляемых бонусных милей за перелет
-        int total_bonus = ticket_price / bonus_miles_value; // Начисляемое количество бонусов за перелет
-        System.out.println(total_bonus);
+        int ticketPrice = 4000; // 4 тыс рублей за билет
+        int bonusMilesValue = 20; // количество начисляемых бонусных милей за перелет
+        int totalBonus = ticketPrice / bonusMilesValue; // Начисляемое количество бонусов за перелет
+        System.out.println(totalBonus);
     }
 }
